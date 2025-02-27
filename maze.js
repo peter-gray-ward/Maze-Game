8cff6f29c186d4bc3c143980e065ec5c6179de60
PeterWard <peterward.pgw@gmail.com>
2025-02-27 10:56:50 -0500

    added additional directions

--- maze.js
+++ maze.js
@@ -3,11 +3,19 @@ const Direction = {
     South: 1,
     East: 2,
     West: 3,
+    NorthEast: 4,
+    SouthEast: 5,
+    SouthWest: 6,
+    NorthWest: 7,
     opposite: dir => {
         if (dir == 0) return 1;
         if (dir == 1) return 0;
         if (dir == 2) return 3;
         if (dir == 3) return 2;
+        if (dir == 4) return 6;
+        if (dir == 5) return 7;
+        if (dir == 6) return 4;
+        if (dir == 7) return 5;
     }
 };
 
@@ -17,7 +25,8 @@ function keyOf(obj, value) {
 
 
 class MapSite {
-    Enter() {}
+    color = "transparent";
+    text = "";
 }
 
 class Side extends MapSite {
@@ -28,7 +37,6 @@ class Side extends MapSite {
         this.direction = direction;
     }
 }
-
 
 class Wall extends Side {
     constructor(direction) {
@@ -42,10 +50,11 @@ class Door extends Side {
     otherSide = null;  // Add reference to matching door
     open = false;
 
-    constructor(direction, roomA, roomB) {
+    constructor(direction, roomA, roomB, color) {
         super(direction);
         this.roomA = roomA;
         this.roomB = roomB;
+        this.color = color;
     }
 }
 
@@ -103,18 +112,16 @@ class MazeGame {
     CreateMaze() {
         var currentRoom = 0;
         var paths = [];
-        const directionStrategy = [Direction.North, Direction.East, Direction.South, Direction.West]; // Trending strategy
-        let currentDirectionIndex = 0; // Start with North
+        const directionStrategy = Object.keys(Direction).filter(key => typeof Direction[key] == 'number').map(key => Direction[key]); // Trending strategy
+        let currentDirectionIndex = 0;
 
         for (var i = 0; i < 1; i++) {
             var path = this.maze.rooms;
-            
+            var x = 0;
             while (currentRoom !== Math.pow(this.dimensions, 2) - 1) {
                 let roomA = path[currentRoom];
                 let nextRoom;
-                let direction = Math.random() < 0.3 ? directionStrategy[currentDirectionIndex] : directionStrategy[Math.floor(Math.random() * this.dimensions)]; // Get the current direction from the strategy
-                let attempts = 0; // Track the number of attempts to find a valid direction
-                const maxAttempts = 3; // Set a maximum number of attempts
+                let direction = Math.random() < 0.3 ? directionStrategy[currentDirectionIndex] : directionStrategy[Math.floor(Math.random() * directionStrategy.length)]; // Get the current direction from the strategy
 
                 if (Math.random() < 0.015) {
                     direction = Direction.opposite(direction);
@@ -124,21 +131,38 @@ class MazeGame {
                     (direction == Direction.West && (currentRoom == 0 || currentRoom % this.dimensions == 0)) ||
                     (direction == Direction.East && currentRoom % this.dimensions == this.dimensions - 1) ||
                     (direction == Direction.South && currentRoom >= this.dimensions * (this.dimensions - 1)) ||
-                    (direction == Direction.North && currentRoom < this.dimensions)
-                ) {
-                    // Switch to the next direction in the strategy
-                    currentDirectionIndex = (currentDirectionIndex + 1) % directionStrategy.length; // Cycle through directions
-                    direction = directionStrategy[currentDirectionIndex]; // Update direction
-                    attempts++;
+                    (direction == Direction.North && currentRoom < this.dimensions) ||
 
-                    // Check if maximum attempts have been reached
-                    if (attempts >= maxAttempts) {
-                        console.log("Max attempts reached, backtracking...", nextRoom, currentRoom);
-                        // Backtrack logic here (if needed)
-                        break; // Exit the loop to prevent infinite loop
-                    }
+                    (direction == Direction.NorthWest 
+                        && (
+                            (currentRoom == 0 || currentRoom % this.dimensions == 0)
+                            || (currentRoom < this.dimensions)
+                        )
+                    ) ||
+                    (direction == Direction.NorthEast 
+                        && (
+                            (currentRoom % this.dimensions == this.dimensions - 1)
+                            || (currentRoom < this.dimensions)
+                        )
+                    ) ||
+                    (direction == Direction.SouthEast 
+                        && (
+                            (currentRoom % this.dimensions == this.dimensions - 1)
+                            || (currentRoom >= this.dimensions * (this.dimensions - 1))
+                        )
+                    ) ||
+                    (direction == Direction.SouthWest 
+                        && (
+                            (currentRoom == 0 || currentRoom % this.dimensions == 0)
+                            || (currentRoom >= this.dimensions * (this.dimensions - 1))
+                        )
+                    )
+                ) {
+                    currentDirectionIndex = (currentDirectionIndex + 1) % directionStrategy.length; // Cycle through directions
+                    direction = directionStrategy[currentDirectionIndex];
                 }
 
+                var rand = Math.random() < 0.5;
                 switch (direction) {
                     case Direction.East:
                         nextRoom = currentRoom + 1;
@@ -152,20 +176,62 @@ class MazeGame {
                     case Direction.North:
                         nextRoom = currentRoom - this.dimensions;
                         break;
+                    case Direction.SouthEast:
+                        if (rand) {
+                            nextRoom = currentRoom + this.dimensions;
+                            direction = Direction.South;
+                        } else {
+                            nextRoom = currentRoom + 1;
+                            direction = Direction.East;
+                        }
+                        break;
+                    case Direction.SouthWest:
+                        if (rand) {
+                            nextRoom = currentRoom + this.dimensions;
+                            direction = Direction.South;
+                        } else {
+                            nextRoom = currentRoom - 1;
+                            direction = Direction.West;
+                        }
+                        break;
+                    case Direction.NorthEast:
+                        if (rand) {
+                            nextRoom = currentRoom - this.dimensions;
+                            direction = Direction.North;
+                        } else {
+                            nextRoom = currentRoom + 1;
+                            direction = Direction.East;
+                        }
+                        break;
+                    case Direction.NorthWest:
+                        if (rand) {
+                            nextRoom = currentRoom - this.dimensions;
+                            direction = Direction.North;
+                        } else {
+                            nextRoom = currentRoom - 1;
+                            direction = Direction.West;
+                        }
+                        break;
                 }
+                
 
                 // Check if nextRoom is valid before accessing roomB
                 if (nextRoom < path.length) {
                     let roomB = path[nextRoom];
 
+                    let adjoiningColor = `rgba(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+
+                    path[currentRoom].color = 'aliceblue';
+                    path[currentRoom].text += `,${x++}`;
+
                     path[currentRoom].SetSide(
                         direction,
-                        new Door(direction, roomA, roomB)
+                        new Door(direction, roomA, roomB, adjoiningColor)
                     );
 
                     path[nextRoom].SetSide(
                         Direction.opposite(direction),
-                        new Door(Direction.opposite(direction), roomB, roomA)
+                        new Door(Direction.opposite(direction), roomB, roomA, adjoiningColor)
                     );
 
                     currentRoom = nextRoom;
@@ -173,6 +239,7 @@ class MazeGame {
                 }
             }
 
+            path[currentRoom].color = 'aliceblue';
         }
 
         for (var path of paths) {
@@ -191,7 +258,8 @@ class MazeGame {
 
 
 require('http').createServer((req, res) => {
-    let game = new MazeGame(12);
+    let dimensions = 23;
+    let game = new MazeGame(dimensions);
     game.CreateMaze();
 
     console.log("created the maze")
@@ -219,8 +287,8 @@ body {
     margin: 0;
 }
 .room {
-    width: calc(100vw / 12 - 1px);
-    height: calc(100vh / 12 - 1px);
+    width: calc(100vw / ${dimensions} - 1px);
+    height: calc(100vh / ${dimensions} - 1px);
     margin: 0;
     box-sizing: border-box;
     border-top: 1px solid;
@@ -268,14 +336,14 @@ body {
 <body>
     ${
         game.maze.rooms.map(room => {
-            return `<div class="room" id="${room.roomNumber.join(",")}">
+            return `<div class="room" id="${room.roomNumber.join(",")}" style="background:${room.color}">
                 ${
                     Object.values(Direction).map(dir => {
                         let side = room.GetSide(dir);
                         if (side instanceof Wall) {
-                            return `<div class="side ${keyOf(Direction, dir)} wall"></div>`;
+                            return `<div class="side ${keyOf(Direction, dir)} wall" style="background:${side.color}"></div>`;
                         } else if (side instanceof Door) {
-                            return `<div class="side ${keyOf(Direction, dir)} door"></div>`;
+                            return `<div class="side ${keyOf(Direction, dir)} door" style="background:${side.color}"></div>`;
                         }
                         return '';
                     }).join('')
